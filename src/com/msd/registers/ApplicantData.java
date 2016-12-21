b3e247093e23b05818bd700e499e9bc6493d6ca1
MenushaMunasinghe <menushamilaj@gmail.com>
2016-12-21 11:50:46 +0530

    Added "forms" package :clap:

---
+++ src/com/msd/registers/ApplicantData.java
@@ -0,0 +1,11 @@
+package com.msd.registers;
+
+import com.msd.pool.Preferences;
+import com.msd.users.Applicant;
+
+public class ApplicantData {
+	
+	private Applicant applicant;
+	private Preferences preferences;
+
+}
