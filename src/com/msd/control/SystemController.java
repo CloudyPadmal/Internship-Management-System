d037f257ad93d5cedd55102b3eac3512b40b1266
CloudyPadmal <blog.padmal@gmail.com>
2016-12-22 07:09:34 +0530

    Added index.jsp

--- src/com/msd/control/SystemController.java
+++ src/com/msd/control/SystemController.java
@@ -1,6 +1,7 @@
 package com.msd.control;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -12,4 +13,11 @@ public class SystemController {
 		String message = "Hello World!!!";
 		return new ModelAndView("hello", "message", message);
 	}
+	
+	@RequestMapping("index")
+	public String loadIndex(ModelMap map) {
+		return "index";
+	}
+	
+	
 }
