7c46af6f0d385172307a001399a919c2212a61d9
Ashira-Jayaweera <ashirajayaweera@gmail.com>
2016-12-28 20:50:34 +0530

    Vacancy.java file created

---
+++ src/com/msd/users/Vacancy.java
@@ -0,0 +1,15 @@
+package com.msd.users;
+
+public class Vacancy {
+
+	private int vacancyID;
+
+	public int getVacancyID() {
+		return vacancyID;
+	}
+
+	public void setVacancyID(int vacancyID) {
+		this.vacancyID = vacancyID;
+	}
+	
+}
