fb8884337ec51ef190037815c4c10a5923ec5a32
Ashira-Jayaweera <ashirajayaweera@gmail.com>
2016-12-28 22:16:06 +0530

    vacancy.java class modified

--- src/com/msd/users/Vacancy.java
+++ src/com/msd/users/Vacancy.java
@@ -1,8 +1,33 @@
 package com.msd.users;
 
+import java.util.List;
+
 public class Vacancy {
 
 	private int vacancyID;
+	private String vacancy;
+	private String title;
+	private String company;
+	private int salary;
+	private List<String> preferences;
+	private String description_1;
+	private String description_2;	
+	
+
+	public Vacancy(int vacancyID, String vacancy, String title, String company, int salary, List<String> preferences,
+			String description_1, String description_2) {
+		super();
+		this.vacancyID = vacancyID;
+		this.vacancy = vacancy;
+		this.title = title;
+		this.company = company;
+		this.salary = salary;
+		this.preferences = preferences;
+		this.description_1 = description_1;
+		this.description_2 = description_2;
+	}
+	
+	
 
 	public int getVacancyID() {
 		return vacancyID;
@@ -12,4 +37,60 @@ public class Vacancy {
 		this.vacancyID = vacancyID;
 	}
 	
+	public String getVacancy() {
+		return vacancy;
+	}
+
+	public void setVacancy(String vacancy) {
+		this.vacancy = vacancy;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public List<String> getPreferences() {
+		return preferences;
+	}
+	
+	public void setPreferences(List<String> preferences) {
+		this.preferences = preferences;
+	}
+	
+	public String getDescription_1() {
+		return description_1;
+	}
+
+	public void setDescription_1(String description_1) {
+		this.description_1 = description_1;
+	}
+
+	public String getDescription_2() {
+		return description_2;
+	}
+
+	public void setDescription_2(String description_2) {
+		this.description_2 = description_2;
+	}
+
 }
